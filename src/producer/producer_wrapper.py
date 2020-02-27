b0f82a925c19f0b194723540b8efbd95ada3b348
ash <ashk365@gmail.com>
2020-02-27 00:18:59 -0500

    Correct stream name that producer will sends records to.

--- src/producer/producer_wrapper.py
+++ src/producer/producer_wrapper.py
@@ -6,7 +6,7 @@ def main():
     data_files_dir = str(sys.argv[1])
     for data_file in os.listdir(data_files_dir):
         csv_file = data_files_dir + '/' + data_file
-        stream_name = data_file.split('.')[0] + "-stream"
+        stream_name = data_file.split('.')[0].replace('_','-') + "-stream"
         p = subprocess.Popen([sys.executable, "producer.py", csv_file, stream_name],
                              stdout=subprocess.PIPE,
                              stderr=subprocess.STDOUT)
