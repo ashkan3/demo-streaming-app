eaf165f5076bdc29963b8c775e6d3a14e4863967
ash <ashk365@gmail.com>
2020-02-27 22:23:47 -0500

    Add source code hash in lambda module to detect lambda code change and deploy new version when possible.

--- infra/modules/kinesis-data-analytics/lambda.tf
+++ infra/modules/kinesis-data-analytics/lambda.tf
@@ -1,16 +1,17 @@
 module lambda {
   source = "github.com/terraform-module/terraform-aws-lambda?ref=v2.9.0"
 
-  function_name  = var.lambda_function_name
-  filename       = data.archive_file.pre_processing_lambda.output_path
-  description    = var.lambda_description
-  handler        = var.lambda_handler
-  runtime        = var.lambda_runtime
-  memory_size    = var.lambda_memory_size
-  concurrency    = var.lambda_concurrency
-  lambda_timeout = var.lambda_timeout
-  log_retention  = var.lambda_log_retention
-  role_arn       = aws_iam_role.lambda_role.arn
+  function_name    = var.lambda_function_name
+  filename         = data.archive_file.pre_processing_lambda.output_path
+  source_code_hash = data.archive_file.pre_processing_lambda.output_sha
+  description      = var.lambda_description
+  handler          = var.lambda_handler
+  runtime          = var.lambda_runtime
+  memory_size      = var.lambda_memory_size
+  concurrency      = var.lambda_concurrency
+  lambda_timeout   = var.lambda_timeout
+  log_retention    = var.lambda_log_retention
+  role_arn         = aws_iam_role.lambda_role.arn
 
   tags = {
     Environment = var.env
