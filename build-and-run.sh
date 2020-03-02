a72b2f4d20203ed4e06cba75da51dbc2081c884d
ash <ashk365@gmail.com>
2020-03-02 00:41:01 -0500

    To deploy everything.

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,4 +1,7 @@
 set -ex
 
-docker build . -t coveo-app
-docker run -it -v /Users/ash/.aws/:/root/.aws -v $(pwd)/data-files:/home/data-files -v $(pwd)/src:/home -v /Users/ash/.ssh:/root/.ssh -v $(pwd)/infra:/home/infra coveo-app bash
+docker build . -t build-tool
+docker run -it -v ~/.aws/:/root/.aws \
+               -v $(pwd)/data-files:/home/data-files \
+               -v $(pwd)/src:/home -v ~/.ssh:/root/.ssh \
+               -v $(pwd)/infra:/home/infra build-tool bash -c "cd /home/infra/dev/apps && terragrunt apply-all"
