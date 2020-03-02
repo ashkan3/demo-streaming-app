909fbe81855aaa0008bbcb395da09f4902efc1b7
ash <ashk365@gmail.com>
2020-03-02 00:51:14 -0500

    Remove unused directories from container runtime.

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,7 +1,7 @@
 set -ex
 
 docker build . -t build-tool
-docker run -it -v ~/.aws/:/root/.aws \
-               -v $(pwd)/data-files:/home/data-files \
-               -v $(pwd)/src:/home -v ~/.ssh:/root/.ssh \
-               -v $(pwd)/infra:/home/infra build-tool bash -c "cd /home/infra/dev/apps && terragrunt apply-all"
+docker run -it \
+           -v ~/.aws/:/root/.aws \
+           -v $(pwd)/infra:/home/infra \
+           build-tool bash -c "cd /home/infra/dev/apps && terragrunt apply-all"
