13304452896d54ba04bc2263872f07abd54dadd6
ash <ashk365@gmail.com>
2020-03-02 23:24:27 -0500

    Pass configuration for desired events to be created to wrapper.

--- infra/dev/apps/producer/wrapper/producer-wrapper.py
+++ infra/dev/apps/producer/wrapper/producer-wrapper.py
@@ -10,11 +10,8 @@ lambda_client = boto3_client('lambda')
 
 def lambda_handler(event, context):
     logger.debug("Lambda was called with event {event}, and context: {context}".format(event=event, context=context))
-    event_types = [{"stream":"custom-events-stream", "bucket": "data-files-demo","file":"custom_events.csv"},
-                    {"stream": "groups-stream", "bucket": "data-files-demo", "file": "groups.csv"}
-                   ]
     invoke_responses = []
-    for event_type in event_types:
+    for event_type in event:
         invoke_responses.append(
             lambda_client.invoke(FunctionName="producer",
                                  InvocationType='Event',
