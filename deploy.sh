dbfe9e7e66df9d23c748a3a4c8c26087693ffbc9
ash <ashk365@gmail.com>
2020-03-02 23:07:32 -0500

    Deploy script should take AWS config directory as an argument, if nothing provided it'll default to ~/.aws .

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 set -ex
 
+AWS_CREDS_DIR=$1
+if [ -z "${AWS_CREDS_DIR}" ];then
+  AWS_CREDS_DIR=~/.aws
+fi
+
 docker build . -t build-tool
 docker run -it \
-           -v ~/.aws/:/root/.aws \
+           -v ${AWS_CREDS_DIR}/:/root/.aws \
            -v $(pwd)/infra:/home/infra \
            build-tool bash -c "cd /home/infra/dev/apps && terragrunt apply-all"
